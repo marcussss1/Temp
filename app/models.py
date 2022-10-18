36da5720b903c38b99a6dc68f44c9b3b4b1f4f6f
marcussss1 <danilalakalash60@gmail.com>
2022-10-18 12:22:02 +0600

    Связал всё друг с другом

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+# class Women(models.Model):
+#     title = models.CharField(max_length=255, verbose_name="Заголовок")
+#     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+#     content = models.TextField(blank=True, verbose_name="Текст статьи")
+#     photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
+#     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+#     time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+#     is_published = models.BooleanField(default=True, verbose_name="Публикация")
+#     cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категории")
+
 # Create your models here.
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -25,6 +25,12 @@ QUESTIONS = [
 
 
 def index(request):
+    # contact_list = QUESTIONS
+    # paginator = Paginator(contact_list, 2)  # Show 25 contacts per page.
+    #
+    # page_number = request.GET.get('page')
+    # page_obj = paginator.get_page(page_number)
+    # return render(request, 'index.html', {'page_obj': page_obj})
     return render(request, "index.html", {"questions": QUESTIONS})
 def new_ask(request):
     return render(request, "new_ask.html", {"questions": QUESTIONS})
